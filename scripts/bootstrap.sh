d7cd9260a8bbce36036772672d6b8eb574897bd6
Takuya ASADA <syuu@cloudius-systems.com>
2015-05-24 08:26:36 +0900

    fix dpdk config, added pciutils and xterm package

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -4,13 +4,13 @@ systemctl restart network
 echo nameserver 8.8.8.8 > /etc/resolv.conf
 useradd -m -p "" -g wheel seastar
 chage -d 0 seastar
-yum install -y gcc gcc-c++ libaio-devel ninja-build ragel hwloc-devel numactl-devel libpciaccess-devel cryptopp-devel xen-devel boost-devel kernel-devel libxml2-devel zlib-devel libasan libubsan git wget python3 tar
+yum install -y gcc gcc-c++ libaio-devel ninja-build ragel hwloc-devel numactl-devel libpciaccess-devel cryptopp-devel xen-devel boost-devel kernel-devel libxml2-devel zlib-devel libasan libubsan git wget python3 tar pciutils xterm
 cd /root
 wget http://dpdk.org/browse/dpdk/snapshot/dpdk-2.0.0.tar.gz
 tar -xpf dpdk-2.0.0.tar.gz
 mv dpdk-2.0.0 dpdk
 cd dpdk
-cat config/common_linuxapp | sed -e "s/CONFIG_RTE_MBUF_REFCNT_ATOMIC=y/CONFIG_RTE_MBUF_REFCNT_ATOMIC=n/g" | sed -e "s/CONFIG_RTE_LIBRTE_PMD_BOND=y/CONFIG_RTE_LIBRTE_PMD_BOND=n/g" | sed -e "s/CONFIG_RTE_LIBRTE_IP_FRAG=y/CONFIG_RTE_LIBRTE_IP_FRAG=n/g" > /tmp/common_linuxapp
+cat config/common_linuxapp | sed -e "s/CONFIG_RTE_MBUF_REFCNT_ATOMIC=y/CONFIG_RTE_MBUF_REFCNT_ATOMIC=n/g" | sed -e "s/CONFIG_RTE_BUILD_SHARED_LIB=n/CONFIG_RTE_BUILD_SHARED_LIB=y/g" > /tmp/common_linuxapp
 mv /tmp/common_linuxapp config
 make T=x86_64-native-linuxapp-gcc install
 cd -
